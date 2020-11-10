ceea0eba96baf54c306a4e60eeffed5f1670b48a
alekseyboglaev <73157803+alekseyboglaev@users.noreply.github.com>
2020-11-10 15:24:53 +0700

    Add files via upload

---
+++ classwork/classwork.cpp
@@ -0,0 +1,78 @@
+﻿// classwork.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    
+
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+
+/*int milk = 3;
+    int chocolate = 4;
+    int coffee = 5;
+    float cost_milk = 104.2;
+    float cost_choc = 23.5;
+    float cost_coff = 45.6;
+    float sum_all = milk * cost_milk + coffee * cost_coff + chocolate * cost_choc;
+    cout << sum_all << endl;*/
+
+/* int milk;
+    cout << "Введите число продуктов: молоко, шоколад, кофе" << endl;
+    cin >> milk;
+    int coffee;
+    cin >> coffee;
+    int chocolate;
+    cin >> chocolate;
+    cout << "Введите цену за штуку: " << endl;
+    float cost_milk{}, cost_choco{}, cost_coff{};
+    cin >> cost_milk, cost_choco, cost_coff;
+    float sum{ 0.0 };
+    sum = chocolate * cost_choco + milk * cost_milk + coffee * cost_milk;
+    cout << sum << endl;*/
+
+/*int num1;
+    int num2;
+    cin >> num1;
+    cin >> num2;
+    int sum;
+    int diff;
+    int pp;
+    float otn;
+    float sr_ar;
+    sum = num1 + num2;
+    cout << sum << endl;
+    diff = num1 - num2;
+    cout << diff << endl;
+    pp = num1 * num2;
+    cout << pp << endl;
+    otn = num1 / num2;
+    cout << otn << endl;
+    sr_ar = (num1 + num2) / 2;
+    cout << sr_ar << endl;*/
+
+/*int num;
+    cin >> num;
+   int a;
+    a =  num % 10;
+    int b;
+    b = (num % 100)/10;
+    int c;
+    c = (num % 1000) / 100;
+    int d;
+    d = num / 1000;
+    cout << a << b << c << d;*/
